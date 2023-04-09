a884bd06fa6e5fd4085c6fa9d56d9da23f868a56
REA5ON <valten007@gmail.com>
2023-04-09 20:34:23 +0200

    added AdminLTE template

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', IndexController::class);
 
+Route::prefix('admin')->group(function () {
+   Route::get('/', \App\Http\Controllers\Admin\IndexController::class);
+});
+
 
 
 
