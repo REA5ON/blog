de8db5e31f8e5229ca12964d103046090998206a
REA5ON <valten007@gmail.com>
2023-04-09 17:28:25 +0200

    added Blog template

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Main\IndexController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', IndexController::class);
+
+
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
