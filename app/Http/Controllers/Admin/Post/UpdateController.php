9366bb29f88922610ac98583570d3b89dce424b8
REA5ON <valten007@gmail.com>
2023-04-26 13:57:08 +0200

    add admin CRUD for making new users

--- app/Http/Controllers/Admin/Post/UpdateController.php
+++ app/Http/Controllers/Admin/Post/UpdateController.php
@@ -2,29 +2,17 @@
 
 namespace App\Http\Controllers\Admin\Post;
 
-use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\Post\UpdateRequest;
 use App\Models\Post;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Support\Facades\Storage;
 
-class UpdateController extends Controller
+class UpdateController extends BaseController
 {
     public function __invoke(UpdateRequest $request, Post $post): RedirectResponse
     {
-        try {
-            $data = $request->validated();
-            $tagsIds = $data['tag_ids'];
-            unset($data['tag_ids']);
+        $data = $request->validated();
 
-            $data['preview_image'] = Storage::disk('public')->put('/images', $data['preview_image']);
-            $data['main_image'] = Storage::disk('public')->put('/images', $data['main_image']);
-
-            $post->update($data);
-            $post->tags()->sync($tagsIds);
-        } catch (\Exception $exception) {
-            abort(404);
-        }
+        $post = $this->service->update($data, $post);
 
         return redirect()->route('admin.post.show', compact('post'))
             ->with('message', 'Post was update successful!');
